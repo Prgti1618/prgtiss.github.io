4db1c57ec162884f1afbf862675d3fd59c871891
Prgti1618 <99989613+Prgti1618@users.noreply.github.com>
2022-07-20 15:05:38 +0530

    Add files via upload

---
+++ contact_form.php
@@ -0,0 +1,18 @@
+
+<?php 
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+$email_form = 'pragatighanekar04@gmail.com';
+$email_subject = "New Form Submission";
+$email_body = "User Name:$name.\n".
+              "User Email:$visitor_email.\n".
+              "User Message:$message.\n".
+
+$to = 'pragatighanekar04@gmail.com';
+$headers ="Form: $email_from \r\n";
+$headers ="Reply-To: $visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+header("Location:index.html");
+?>
