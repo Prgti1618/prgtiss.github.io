90adcdd0b1bfbefd86f1a48971eb61a3d827ae9b
Prgti1618 <99989613+Prgti1618@users.noreply.github.com>
2022-08-12 12:26:07 +0530

    Add files via upload

---
+++ prindo.js
@@ -0,0 +1,306 @@
+
+
+
+function oneAnim(){
+   var my_static_element = document.querySelector(".naming");
+   let classesToremove = [ 'two_anim', 'three_anim', 'four_anim', 'five_anim', 'six_anim', 'seven_anim', 'eight_anim', 'nine_anim', 'ten_anim', 'eleven_anim', 'twelve_anim', 'thirteen_anim', 'fourteen_anim', 'fifteen_anim' ];
+   my_static_element.classList.remove(...classesToremove);
+   my_static_element.classList.add("one_anim");
+   // my_box_element = document.querySelector(".mybox");
+   var codesnippet_box_element1 = document.querySelector(".codesnippet_box1");
+
+   const nodeList = document.querySelectorAll(".mybox");
+for (let i = 0; i < nodeList.length; i++) {
+  nodeList[i].style.display = "none";
+}
+      codesnippet_box_element1.style.display = "block";
+
+   }
+
+
+function twoAnim() {
+   var my_static_element = document.querySelector(".naming");
+   let classesToremove = [ 'one_anim', 'three_anim', 'four_anim', 'five_anim', 'six_anim', 'seven_anim', 'eight_anim', 'nine_anim', 'ten_anim', 'eleven_anim', 'twelve_anim', 'thirteen_anim', 'fourteen_anim', 'fifteen_anim' ];
+   my_static_element.classList.remove(...classesToremove);
+   my_static_element.classList.add("two_anim");
+   var codesnippet_box_element2 = document.querySelector(".codesnippet_box2");
+   const nodeList = document.querySelectorAll(".mybox");
+   for (let i = 0; i < nodeList.length; i++) {
+     nodeList[i].style.display = "none";
+   }
+         codesnippet_box_element2.style.display = "block";
+   
+      }
+  
+    
+
+
+
+function threeAnim() {
+   var my_static_element = document.querySelector(".naming");
+   let classesToremove = [ 'two_anim', 'one_anim', 'four_anim', 'five_anim', 'six_anim', 'seven_anim', 'eight_anim', 'nine_anim', 'ten_anim', 'eleven_anim', 'twelve_anim', 'thirteen_anim', 'fourteen_anim', 'fifteen_anim' ];
+   my_static_element.classList.remove(...classesToremove);
+   var codesnippet_box_element3 = document.querySelector(".codesnippet_box3");
+   my_static_element.classList.add("three_anim");
+   const nodeList = document.querySelectorAll(".mybox");
+   for (let i = 0; i < nodeList.length; i++) {
+     nodeList[i].style.display = "none";
+   }
+         codesnippet_box_element3.style.display = "block";
+   
+      }
+
+
+
+function fourAnim() {
+   var my_static_element = document.querySelector(".naming");
+   let classesToremove = [ 'two_anim', 'one_anim', 'three_anim', 'five_anim', 'six_anim', 'seven_anim', 'eight_anim', 'nine_anim', 'ten_anim', 'eleven_anim', 'twelve_anim', 'thirteen_anim', 'fourteen_anim', 'fifteen_anim' ];
+   my_static_element.classList.remove(...classesToremove);
+   var codesnippet_box_element4 = document.querySelector(".codesnippet_box4");
+   my_static_element.classList.add("four_anim");
+   const nodeList = document.querySelectorAll(".mybox");
+   for (let i = 0; i < nodeList.length; i++) {
+     nodeList[i].style.display = "none";
+   }
+         codesnippet_box_element4.style.display = "block";
+   
+      }
+
+
+function fiveAnim() {
+   var my_static_element = document.querySelector(".naming");
+   let classesToremove = [ 'two_anim', 'one_anim', 'four_anim', 'three_anim', 'six_anim', 'seven_anim', 'eight_anim', 'nine_anim', 'ten_anim', 'eleven_anim', 'twelve_anim', 'thirteen_anim', 'fourteen_anim', 'fifteen_anim' ];
+   my_static_element.classList.remove(...classesToremove);
+   var codesnippet_box_element5 = document.querySelector(".codesnippet_box5");
+   my_static_element.classList.add("five_anim");
+   const nodeList = document.querySelectorAll(".mybox");
+   for (let i = 0; i < nodeList.length; i++) {
+     nodeList[i].style.display = "none";
+   }
+         codesnippet_box_element5.style.display = "block";
+   
+      }
+
+
+function sixAnim() {
+   var my_static_element = document.querySelector(".naming");
+   let classesToremove = [ 'two_anim', 'one_anim', 'four_anim', 'five_anim', 'three_anim', 'seven_anim', 'eight_anim', 'nine_anim', 'ten_anim', 'eleven_anim', 'twelve_anim', 'thirteen_anim', 'fourteen_anim', 'fifteen_anim' ];
+   my_static_element.classList.remove(...classesToremove);
+   var codesnippet_box_element6 = document.querySelector(".codesnippet_box6");
+   my_static_element.classList.add("six_anim");
+   const nodeList = document.querySelectorAll(".mybox");
+   for (let i = 0; i < nodeList.length; i++) {
+     nodeList[i].style.display = "none";
+   }
+         codesnippet_box_element6.style.display = "block";
+   
+
+
+}
+function sevenAnim() {
+   var my_static_element = document.querySelector(".naming");
+   let classesToremove = [ 'two_anim', 'one_anim', 'four_anim', 'five_anim', 'six_anim', 'three_anim', 'eight_anim', 'nine_anim', 'ten_anim', 'eleven_anim', 'twelve_anim', 'thirteen_anim', 'fourteen_anim', 'fifteen_anim' ];
+   my_static_element.classList.remove(...classesToremove);
+   var codesnippet_box_element7 = document.querySelector(".codesnippet_box7");
+   my_static_element.classList.add("seven_anim");
+   const nodeList = document.querySelectorAll(".mybox");
+   for (let i = 0; i < nodeList.length; i++) {
+     nodeList[i].style.display = "none";
+   }
+         codesnippet_box_element7.style.display = "block";
+   
+
+
+}
+function eightAnim() {
+   var my_static_element = document.querySelector(".naming");
+   let classesToremove = [ 'two_anim', 'one_anim', 'four_anim', 'five_anim', 'six_anim', 'seven_anim', 'three_anim', 'nine_anim', 'ten_anim', 'eleven_anim', 'twelve_anim', 'thirteen_anim', 'fourteen_anim', 'fifteen_anim' ];
+   my_static_element.classList.remove(...classesToremove);
+   var codesnippet_box_element8 = document.querySelector(".codesnippet_box8");
+   my_static_element.classList.add("eight_anim");
+   const nodeList = document.querySelectorAll(".mybox");
+   for (let i = 0; i < nodeList.length; i++) {
+     nodeList[i].style.display = "none";
+   }
+         codesnippet_box_element8.style.display = "block";
+   
+
+
+}
+function nineAnim() {
+   var my_static_element = document.querySelector(".naming");
+   let classesToremove = [ 'two_anim', 'one_anim', 'four_anim', 'five_anim', 'six_anim', 'seven_anim', 'eight_anim', 'three_anim', 'ten_anim', 'eleven_anim', 'twelve_anim', 'thirteen_anim', 'fourteen_anim', 'fifteen_anim' ];
+   my_static_element.classList.remove(...classesToremove);
+   var codesnippet_box_element9 = document.querySelector(".codesnippet_box9");
+   my_static_element.classList.add("nine_anim");
+   const nodeList = document.querySelectorAll(".mybox");
+   for (let i = 0; i < nodeList.length; i++) {
+     nodeList[i].style.display = "none";
+   }
+         codesnippet_box_element9.style.display = "block";
+   
+
+}
+function tenAnim() {
+   var my_static_element = document.querySelector(".naming");
+   let classesToremove = [ 'two_anim', 'one_anim', 'four_anim', 'five_anim', 'six_anim', 'seven_anim', 'eight_anim', 'nine_anim', 'three_anim', 'eleven_anim', 'twelve_anim', 'thirteen_anim', 'fourteen_anim', 'fifteen_anim' ];
+   my_static_element.classList.remove(...classesToremove);
+   var codesnippet_box_element10 = document.querySelector(".codesnippet_box10");
+   my_static_element.classList.add("ten_anim");
+   const nodeList = document.querySelectorAll(".mybox");
+   for (let i = 0; i < nodeList.length; i++) {
+     nodeList[i].style.display = "none";
+   }
+         codesnippet_box_element10.style.display = "block";
+   
+
+}
+function elevenAnim() {
+   var my_static_element = document.querySelector(".naming");
+   let classesToremove = [ 'two_anim', 'one_anim', 'four_anim', 'five_anim', 'six_anim', 'seven_anim', 'eight_anim', 'nine_anim', 'ten_anim', 'three_anim', 'twelve_anim', 'thirteen_anim', 'fourteen_anim', 'fifteen_anim' ];
+   my_static_element.classList.remove(...classesToremove);
+   var codesnippet_box_element11 = document.querySelector(".codesnippet_box11");
+   my_static_element.classList.add("eleven_anim");
+   const nodeList = document.querySelectorAll(".mybox");
+   for (let i = 0; i < nodeList.length; i++) {
+     nodeList[i].style.display = "none";
+   }
+         codesnippet_box_element11.style.display = "block";
+   
+
+}
+function twelveAnim() {
+   var my_static_element = document.querySelector(".naming");
+   let classesToremove = [ 'two_anim', 'one_anim', 'four_anim', 'five_anim', 'six_anim', 'seven_anim', 'eight_anim', 'nine_anim', 'ten_anim', 'eleven_anim', 'three_anim', 'thirteen_anim', 'fourteen_anim', 'fifteen_anim' ];
+   my_static_element.classList.remove(...classesToremove);
+   var codesnippet_box_element12 = document.querySelector(".codesnippet_box12");
+   my_static_element.classList.add("twelve_anim");
+   const nodeList = document.querySelectorAll(".mybox");
+   for (let i = 0; i < nodeList.length; i++) {
+     nodeList[i].style.display = "none";
+   }
+         codesnippet_box_element12.style.display = "block";
+   
+
+}
+function thirteenAnim() {
+   var my_static_element = document.querySelector(".naming");
+   let classesToremove = [ 'two_anim', 'one_anim', 'four_anim', 'five_anim', 'six_anim', 'seven_anim', 'eight_anim', 'nine_anim', 'ten_anim', 'eleven_anim', 'twelve_anim', 'three_anim', 'fourteen_anim', 'fifteen_anim' ];
+   my_static_element.classList.remove(...classesToremove);
+   var codesnippet_box_element13 = document.querySelector(".codesnippet_box13");
+   my_static_element.classList.add("thirteen_anim");
+   const nodeList = document.querySelectorAll(".mybox");
+   for (let i = 0; i < nodeList.length; i++) {
+     nodeList[i].style.display = "none";
+   }
+         codesnippet_box_element13.style.display = "block";
+   
+
+}
+function fourteenAnim() {
+   var my_static_element = document.querySelector(".naming");
+   let classesToremove = [ 'two_anim', 'one_anim', 'four_anim', 'five_anim', 'six_anim', 'seven_anim', 'eight_anim', 'nine_anim', 'ten_anim', 'eleven_anim', 'twelve_anim', 'thirteen_anim', 'three_anim', 'fifteen_anim' ];
+   my_static_element.classList.remove(...classesToremove);
+   var codesnippet_box_element14 = document.querySelector(".codesnippet_box14");
+   my_static_element.classList.add("fourteen_anim");
+   const nodeList = document.querySelectorAll(".mybox");
+   for (let i = 0; i < nodeList.length; i++) {
+     nodeList[i].style.display = "none";
+   }
+         codesnippet_box_element14.style.display = "block";
+   
+
+}
+function fifteenAnim(){  
+    var my_static_element = document.querySelector(".naming");
+   let classesToremove = [ 'two_anim', 'one_anim', 'four_anim', 'five_anim', 'six_anim', 'seven_anim', 'eight_anim', 'nine_anim', 'ten_anim', 'eleven_anim', 'twelve_anim', 'thirteen_anim', 'fourteen_anim', 'three_anim' ];
+   my_static_element.classList.remove(...classesToremove);
+   var codesnippet_box_element15 = document.querySelector(".codesnippet_box15");
+   my_static_element.classList.add("fifteen_anim");
+   const nodeList = document.querySelectorAll(".mybox");
+   for (let i = 0; i < nodeList.length; i++) {
+     nodeList[i].style.display = "none";
+   }
+         codesnippet_box_element15.style.display = "block";
+   
+
+}
+
+
+
+    // function show_hidden(){
+    
+    function myFunction() {
+  var x = document.getElementById("sidenavid");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+const mediaQuery = window.matchMedia('(max-width:950px)')
+if(mediaQuery.matches){
+function mytwoFunction(){
+document.getElementById("sidenavid").style.display = "none";
+}
+}
+
+function showTerms(){
+const terms = document.querySelector(".terms_bottom");
+const privacy = document.querySelector(".privacy_bottom");
+const contact = document.querySelector(".contact_bottom");
+
+const termslist = document.querySelector(".termsli");
+if(terms.style.display ==="none"){
+  terms.style.display ="block";
+  privacy.style.display ="none";
+  contact.style.display ="none";
+  termslist.style.color = "#db4a39";
+ 
+}else{
+  terms.style.display ="none";
+  termslist.style.color = "inherit";
+
+}
+}
+
+function showPrivacy(){
+  const terms = document.querySelector(".terms_bottom");
+  const privacy = document.querySelector(".privacy_bottom");
+  const contact = document.querySelector(".contact_bottom");
+  const privacylist = document.querySelector(".privacyli");
+
+  if(privacy.style.display ==="none"){
+    privacy.style.display ="block";
+    terms.style.display ="none";
+    contact.style.display ="none";
+   
+    privacylist.style.color = "#db4a39";
+  }else{
+    privacy.style.display ="none";
+    privacylist.style.color = "inherit";
+   
+  }
+  }
+
+  function showContact(){
+    const terms = document.querySelector(".terms_bottom");
+    const privacy = document.querySelector(".privacy_bottom");
+    const contact = document.querySelector(".contact_bottom");
+    const contactlist = document.querySelector(".contactli");
+    if(contact.style.display ==="none"){
+      contact.style.display ="block";
+      privacy.style.display ="none";
+      terms.style.display ="none";
+     
+      contactlist.style.color = "#db4a39";
+    }else{
+      contact.style.display ="none";
+      contactlist.style.color = "inherit";
+     
+      
+    
+    }
+    }
